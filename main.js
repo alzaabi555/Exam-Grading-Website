26ffa7e493bd6fc96021a79189d18d9f9891c423
alzaabi555 <157557210+alzaabi555@users.noreply.github.com>
2026-02-27 15:39:16 +0400

    Update main.js

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    icon: path.join(__dirname, 'public/icon.ico'),
+    icon: path.join(__dirname, 'public/icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -42,3 +42,4 @@ app.whenReady().then(createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
